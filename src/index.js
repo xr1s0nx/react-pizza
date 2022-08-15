edd555c028a24bb3ead9ae420357cd467b50bd4b
Artem <artem.gumerov.05@gmail.com>
2022-08-16 00:10:46 +0300

    add redux

--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import {
   BrowserRouter
 } from "react-router-dom";
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider value={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+
   </React.StrictMode>
 );
 reportWebVitals();
